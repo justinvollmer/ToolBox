0caa9f74412948e8dddbb883251a7a08726f736f
Justin Vollmer <justinvollmer01@googlemail.com>
2023-08-28 04:02:38 +0200

    Add: LinkManager menu

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BaseLayout from "./components/BaseLayout";
 import Home from "./views/Home/Home";
 import About from "./views/About/About";
+import LinkManager from "./views/LinkManager/LinkManager";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<BaseLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="linkmanager" element={null} />
+          <Route path="linkmanager" element={<LinkManager />} />
           <Route path="watchlist" element={null} />
           <Route path="events" element={null} />
         </Route>
