4d174133490cc536f4c23631c4aaa6ff515329b3
JustinVollmer <justinvollmer01@googlemail.com>
2023-11-10 00:33:20 +0100

    Initialized Electron

---
+++ src/main.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+const { app, BrowserWindow } = require("electron");
+
+// modify your existing createWindow() function
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL("http://localhost:5173/");
+  win.webContents.openDevTools();
+};
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
