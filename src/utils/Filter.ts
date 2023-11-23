a8a7e23cb4d3af34db8133dd6ccfe6517bc56825
Justin Vollmer <89389760+justinvollmer@users.noreply.github.com>
2023-11-23 11:26:49 +0100

    fix

--- src/utils/Filter.ts
+++ src/utils/Filter.ts
@@ -4,3 +4,15 @@ function splitLines(text: string): string[] {
   const splittedText: string[] = text.split("\n");
   return splittedText;
 }
+
+function removeEmptyLines(text: string | string[]): string {
+  if (typeof text == "string") {
+    console.log("ist String");
+  } else if (Array.isArray(text)) {
+    console.log("ist String[]");
+  }
+
+  return "nichts";
+}
+
+export { splitLines, removeEmptyLines };
